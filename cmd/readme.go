3435d582f23fa3cb5afafca8d5239686bad3e7f7
Jeff Willette <jrwillette88@gmail.com>
2017-09-26 12:41:26 +0900

    Enabled colors by default
    - color library usually checks for terminal and disables colors
    - with colors enabled by default, user can pipe to `less -R`
    - added print condition for inline code blocks
    - added new readme gif

--- cmd/readme.go
+++ cmd/readme.go
@@ -18,12 +18,48 @@ var (
 	// blocks can look a lot like mardown headers.
 	c = regexp.MustCompile("```")
 
+	// regex for inline code
+	ic = regexp.MustCompile("`(.*?)`")
+
 	header = color.New(color.FgMagenta, color.Bold, color.Underline, color.Italic)
 	code   = color.New(color.FgBlue, color.Faint)
 )
 
+// EnableColors causes the colors to be enabled by default. The color library usually
+// tests to see if the stdout is a terminal first and disables them if not. With colors
+// always enabled, the user can pipe to `less -R` and still get color output
+func EnableColors() {
+	header.EnableColor()
+	code.EnableColor()
+}
+
+// startCodeBlock returns true if it was not previously a code block and the regex matches
+// the beginning of a code block.
+func startCodeBlock(was, match bool) bool {
+	return !was && match
+}
+
+// endCodeBlock returns true if it was previously a code block and the regex matches the
+// code block closign tag.
+func endCodeBlock(was, match bool) bool {
+	return was && match
+}
+
+// PrintInlineCode prints when there is code in between backticks (`code`)
+func PrintInlineCode(s string) {
+	print := code.SprintFunc()
+	inlineCodedString := ic.ReplaceAllStringFunc(s, func(foo string) string {
+		return print(foo)
+	})
+
+	fmt.Println(inlineCodedString)
+}
+
 // Readme gets and prints the file to the terminal
 func Readme(s []string) {
+
+	EnableColors()
+
 	split := strings.Split(s[0], "/")
 
 	domain := "http://raw.githubusercontent.com"
@@ -50,21 +86,23 @@ func Readme(s []string) {
 		t := scanner.Text()
 
 		wasCodeBlock := isCodeBlock
-		if !wasCodeBlock && c.MatchString(t) {
+		if startCodeBlock(wasCodeBlock, c.MatchString(t)) {
 			isCodeBlock = true
 		}
 
 		switch {
-		case r.MatchString(t) && !isCodeBlock:
-			header.Print(t)
-			fmt.Print("\n\n")
 		case isCodeBlock:
 			code.Println(t)
+		case r.MatchString(t):
+			header.Print(t)
+			fmt.Print("\n\n")
+		case ic.MatchString(t):
+			PrintInlineCode(t)
 		default:
 			fmt.Println(t)
 		}
 
-		if wasCodeBlock && c.MatchString(t) {
+		if endCodeBlock(wasCodeBlock, c.MatchString(t)) {
 			isCodeBlock = false
 		}
 	}
